227dc5767a26834a8c7825e1dcf366ce7d5980e8
Kim JI woon <kishe56@gmail.com>
2016-08-19 18:14:00 +0900

    Excel파싱해서 json파일로 출력하는 소스

---
+++ app.js
@@ -0,0 +1,19 @@
+/**
+ * http://usejsdoc.org/
+ */
+var Pull = require('excel-push-pull').Pull;
+var pull = new Pull();
+var fs = require('fs');
+var jsonfile = require('jsonfile');
+pull.setFilePath('./excel/spotdata.xlsx');
+pull.records(function(err, records) {
+  // records is an array contains json data 
+	if(err){
+		console.log(err);
+	}
+	console.log("records length = " + records.length);
+	var file = './json/kr.json';
+	jsonfile.writeFile(file, records, function (err) {
+	  console.error(err);
+	});
+});
