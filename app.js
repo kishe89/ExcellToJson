e687a64428af80ada80517596ea29ea722e48595
Kim JI woon <kishe56@gmail.com>
2016-08-24 04:17:57 +0900

    파싱로직 변경

--- app.js
+++ app.js
@@ -5,10 +5,10 @@
 var jsonfile1 = require('jsonfile');
 var xlsx = require('node-xlsx');
 var filepath = ['./json/kr.json','./json/eng.json','./json/jp.json','./json/ch.json'];
-const workSheetsFromFile = xlsx.parse(`./excel/kr.xlsx`);
+const workSheetsFromFile = xlsx.parse(`./excel/경주스탬프2.0.xlsx`);
 
 function parser_header(workSheetno){
-	var first = workSheetsFromFile[workSheetno].data[0].join();
+	var first = workSheetsFromFile[workSheetno].data[2].join();
 	var headers = first.split(',');
 	var key = new Array();
 	for(var i = 0;i<headers.length;i++){
@@ -20,7 +20,7 @@ function parser_header(workSheetno){
 }
 function parser_body(workSheetno,key){
 	var jsonData = [];
-	for(var j = 1 ; j<workSheetsFromFile[workSheetno].data.length ;j++){
+	for(var j = 3 ; j<workSheetsFromFile[workSheetno].data.length ;j++){
 		var firstrows = workSheetsFromFile[workSheetno].data[j].join();
 		var rows = firstrows;
 		var test = new Array();
@@ -33,7 +33,7 @@ function parser_body(workSheetno,key){
 				end=rows.indexOf(',');
 				temp = rows.substring(start, end);
 			}
-			else if(i<5){
+			else if(i<7){
 				start= end+1;
 				end= rows.indexOf(',',start);
 				temp = rows.substring(start, end);
@@ -59,13 +59,13 @@ function parser_body(workSheetno,key){
 }
 function write_json(){
 	console.log(workSheetsFromFile.length);
-	for(var k = 0 ; k<workSheetsFromFile.length;k++){
+	for(var k = 1 ; k<workSheetsFromFile.length;k++){
 		var key = parser_header(k);
 		var res_json = parser_body(k,key);
 		console.log(key);
 		console.log(res_json);
 
-		var file = filepath[k];
+		var file = filepath[k-1];
 		jsonfile1.writeFile(file, res_json, function (err) {
 		  console.error(err);
 		});
